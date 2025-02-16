5a061852eec70614d807f9cbadce8213e9907963
Yida Wang <yidawa@gmail.com>
2025-02-16 13:50:02 -0800

    put dir into build and fix a dir issue in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  outDir: 'dist',
-  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   base: '/haymaker-studio/',
   optimizeDeps: {
     exclude: ['lucide-react'],
