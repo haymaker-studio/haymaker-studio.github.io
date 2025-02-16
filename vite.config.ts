3fd89f8ac2f22eb009b72db9f61e68ba435c2c5b
Yida Wang <yidawa@gmail.com>
2025-02-16 13:11:29 -0800

    restore to the original code with the correct base format

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  build: {
-    assetsDir: 'assets',
-    modulePreload: false,
-    cssCodeSplit: true,
-    sourcemap: false
-  },
   plugins: [react()],
-})
+  base: '/haymaker-studio/'
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
